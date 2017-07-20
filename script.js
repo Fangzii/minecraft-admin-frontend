47883e98c3e174a5aa33b1ba9828b21815792c1a
Fanggg <467655981@qq.com>
2017-07-20 00:43:31 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 /**
  * Created by admin on 2017/7/19.
  */
+function pagenum() {
 
-var  start = ctrl.loadCommands().start,
-     count = ctrl.loadCommands().count,
-     total = ctrl.loadCommands().total;
 
-var pagenum = total/count ,
-    page = start/count ;
-document.getElementById("demo").innerHTML=page;
+
+
+}
