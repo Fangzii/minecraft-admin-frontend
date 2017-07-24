0eef141c6b64c031af5e3b331f56a8f971d6aed0
Fanggg <467655981@qq.com>
2017-07-24 01:34:05 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -149,10 +149,30 @@ angular.module('minecraft', [])
                 // or server returns response with an error status.
             });
         }
-        this.Burn= function (username) {
+        this.Burn= function (username,burntime) {
             $http({
-                method: 'post',
-                url: 'http://nas.fangs.work:25585/users/'+username+'/do/burn'
+                method: 'patch',
+                url: 'http://nas.fangs.work:25585/users/'+username+'/do/burn',
+                data: {seconds: burntime}
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                self.currentUserDetail = response.data
+
+
+
+
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+        }
+
+        this.Exp= function (username) {
+            $http({
+                method: 'patch',
+                url: 'http://nas.fangs.work:25585/users/'+username
+
             }).then(function successCallback(response) {
                 // this callback will be called asynchronously
                 // when the response is available
