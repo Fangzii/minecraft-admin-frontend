154de7a3912ba5d5aaa94133a5470ed0e3d532c7
Fanggg <467655981@qq.com>
2017-07-25 02:23:49 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -153,11 +153,15 @@ angular.module('minecraft', [])
             $http({
                 method: 'patch',
                 url: 'http://nas.fangs.work:25585/users/'+username+'/do/burn',
-                data: {seconds: burntime}
+                data: {"seconds": burntime},
+               header: {
+                'content-type': 'application/json'
+            }
             }).then(function successCallback(response) {
                 // this callback will be called asynchronously
                 // when the response is available
                 self.currentUserDetail = response.data
+                console.log(self.Burn)
 
 
 
@@ -168,15 +172,19 @@ angular.module('minecraft', [])
             });
         }
 
-        this.Exp= function (username) {
+        this.Exp= function (username,num) {
             $http({
                 method: 'patch',
-                url: 'http://nas.fangs.work:25585/users/'+username
-
+                url: 'http://nas.fangs.work:25585/users/'+username,
+                data: {
+                    "exp":num
+                },
+                header: {
+                    'content-type': 'application/json'
+                }
             }).then(function successCallback(response) {
                 // this callback will be called asynchronously
                 // when the response is available
-                self.currentUserDetail = response.data
 
 
 
@@ -186,6 +194,55 @@ angular.module('minecraft', [])
                 // or server returns response with an error status.
             });
         }
+
+        this.Op= function (username,num) {
+            $http({
+                method: 'patch',
+                url: 'http://nas.fangs.work:25585/users/'+username,
+                data: {
+                    "op":num
+                },
+                header: {
+                    'content-type': 'application/json'
+                }
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+
+
+
+
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+        }
+
+
+        this.Mod= function (username,num) {
+            $http({
+                method: 'patch',
+                url: 'http://nas.fangs.work:25585/users/'+username,
+                data: {
+                    "gamemode":"'"+num+"'"
+                },
+                header: {
+                    'content-type': 'application/json'
+                }
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+
+
+
+
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+        }
+
+
 
     })
 
