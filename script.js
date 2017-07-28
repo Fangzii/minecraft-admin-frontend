1d231fe45cb514eccf84f4f7b06ad3cbae58abe8
Fanggg <467655981@qq.com>
2017-07-28 00:49:28 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-/**
- * Created by admin on 2017/7/19.
- */
-function pagenum() {
+'use strict';
 
+var house = document.querySelector('#house');
+var range = document.querySelector('#range');
+var label = document.querySelector('#label');
 
+var f = new Flipping();
+var update = f.wrap(function (rooms) {
+    var prevRooms = house.getAttribute('data-rooms');
+    house.setAttribute('data-prev-rooms', prevRooms);
+    house.setAttribute('data-rooms', rooms);
 
+    label.setAttribute('data-prev-rooms', prevRooms);
+    label.setAttribute('data-rooms', rooms);
+    label.setAttribute('data-rooms-delta', rooms - prevRooms);
+});
+range.addEventListener('input', function (e) {
+    return update(e.target.value);
+});
 
-}
+// start with 6
+update(6);
